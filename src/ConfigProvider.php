29934f610d475dc4ccd82f36186be94120d086ed
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-15 15:24:56 +0100

    CS

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * kiwi-suite/database (https://github.com/kiwi-suite/database)
+ *
+ * @package kiwi-suite/database
+ * @see https://github.com/kiwi-suite/database
+ * @copyright Copyright (c) 2010 - 2017 kiwi suite GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
 namespace KiwiSuite\Database;
 
 use KiwiSuite\Contract\Application\ConfigProviderInterface;

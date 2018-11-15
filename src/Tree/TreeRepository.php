433bf40bfe2dcfeb63437b6db9c6c0bc497d6748
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2018-11-15 12:49:56 +0100

    clear EntityManager after changing nested set, add nested set checks

--- src/Tree/TreeRepository.php
+++ src/Tree/TreeRepository.php
@@ -3,11 +3,51 @@ declare(strict_types=1);
 
 namespace KiwiSuite\Database\Tree;
 
+use KiwiSuite\Database\Exception\InvalidArgumentException;
 use KiwiSuite\Database\Repository\AbstractRepository;
 use KiwiSuite\Entity\Entity\EntityInterface;
 
 abstract class TreeRepository extends AbstractRepository
 {
+    private $leftParameterName;
+    private $rightParameterName;
+    private $parentIdParameterName;
+
+    public function leftParameterName()
+    {
+        if ($this->leftParameterName === null) {
+            $reflection = new \ReflectionClass($this->getEntityName());
+            $instance = $reflection->newInstanceWithoutConstructor();
+            $this->leftParameterName = $instance->leftParameterName();
+            $this->rightParameterName = $instance->rightParameterName();
+            $this->parentIdParameterName = $instance->parentIdParameterName();
+        }
+        return $this->leftParameterName;
+    }
+
+    public function rightParameterName()
+    {
+        if ($this->rightParameterName === null) {
+            $reflection = new \ReflectionClass($this->getEntityName());
+            $instance = $reflection->newInstanceWithoutConstructor();
+            $this->leftParameterName = $instance->leftParameterName();
+            $this->rightParameterName = $instance->rightParameterName();
+            $this->parentIdParameterName = $instance->parentIdParameterName();
+        }
+        return $this->rightParameterName;
+    }
+
+    public function parentIdParameterName()
+    {
+        if ($this->parentIdParameterName === null) {
+            $reflection = new \ReflectionClass($this->getEntityName());
+            $instance = $reflection->newInstanceWithoutConstructor();
+            $this->leftParameterName = $instance->leftParameterName();
+            $this->rightParameterName = $instance->rightParameterName();
+            $this->parentIdParameterName = $instance->parentIdParameterName();
+        }
+        return $this->parentIdParameterName;
+    }
 
     /**
      * @param NodeInterface $node
@@ -16,7 +56,6 @@ abstract class TreeRepository extends AbstractRepository
     public function createRoot(NodeInterface $node): NodeInterface
     {
         $left = 1;
-
 
         $result = $this->findBy([], [$node->rightParameterName() => 'DESC'], 1);
         if (!empty($result)) {
@@ -54,7 +93,9 @@ abstract class TreeRepository extends AbstractRepository
         $right = $left + 1;
 
         $this->shiftNestedRange($node, $left, 0, 2);
-        return $this->insertNode($node, $left, $right, $sibling->parentId());
+        $node = $this->insertNode($node, $left, $right, $sibling->parentId());
+        $this->getEntityManager()->clear($this->getEntityName());
+        return $node;
     }
 
     /**
@@ -68,7 +109,9 @@ abstract class TreeRepository extends AbstractRepository
         $right = $left + 1;
 
         $this->shiftNestedRange($node, $left, 0, 2);
-        return $this->insertNode($node, $left, $right, $parent->id());
+        $node = $this->insertNode($node, $left, $right, $parent->id());
+        $this->getEntityManager()->clear($this->getEntityName());
+        return $node;
     }
 
     /**
@@ -82,7 +125,9 @@ abstract class TreeRepository extends AbstractRepository
         $right = $left + 1;
 
         $this->shiftNestedRange($node, $left, 0, 2);
-        return $this->insertNode($node, $left, $right, $parent->id());
+        $node = $this->insertNode($node, $left, $right, $parent->id());
+        $this->getEntityManager()->clear($this->getEntityName());
+        return $node;
     }
 
     /**
@@ -145,6 +190,8 @@ abstract class TreeRepository extends AbstractRepository
         $first = $right + 1;
         $delta = $left - $right - 1;
         $this->shiftNestedRange($entity, $first, 0, $delta);
+
+        $this->getEntityManager()->clear($this->getEntityName());
     }
 
     /**
@@ -171,12 +218,16 @@ abstract class TreeRepository extends AbstractRepository
      */
     private function updateNode(NodeInterface $node, int $destinationLeft, $parent): NodeInterface
     {
-        $node = $node->with($node->parentIdParameterName(), $parent);
-        $node = $this->save($node);
-
         $left = $node->left();
         $right = $node->right();
         $size = $right - $left + 1;
+
+        if ($destinationLeft >= $left && $destinationLeft <= $right) {
+            throw new InvalidArgumentException('node can not be its own child');
+        }
+
+        $node = $node->with($node->parentIdParameterName(), $parent);
+        $node = $this->save($node);
 
         $this->shiftNestedRange($node, $destinationLeft, 0, $size);
 
@@ -187,6 +238,8 @@ abstract class TreeRepository extends AbstractRepository
 
         $this->shiftNestedRange($node, $left, $right, $destinationLeft - $left);
         $this->shiftNestedRange($node, $right + 1, 0, $size * -1);
+
+        $this->getEntityManager()->clear($this->getEntityName());
 
         return $node;
     }
@@ -214,5 +267,110 @@ abstract class TreeRepository extends AbstractRepository
 
             $queryBuilder->getQuery()->execute();
         }
+    }
+
+    /**
+     * @return int
+     */
+    public function getOddnessCount() :int
+    {
+        $left = $this->leftParameterName();
+        $right = $this->rightParameterName();
+
+        $queryBuilder = $this->createQueryBuilder();
+        $queryBuilder
+            ->select('count(1) as count')
+            ->from($this->getEntityName(), 'node')
+            ->orWhere("node.$left >= node.$right")
+            ->orWhere("mod(node.$right - node.$left, 2) = 0");
+
+        $result = $queryBuilder->getQuery()->execute();
+        return (int) $result[0]['count'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getDuplicateCount() :int
+    {
+        $left = $this->leftParameterName();
+        $right = $this->rightParameterName();
+
+        $queryBuilder = $this->createQueryBuilder();
+        $queryBuilder
+            ->select('count(1) as count')
+            ->from($this->getEntityName(), 'n1')
+            ->from($this->getEntityName(), 'n2')
+            ->andWhere('n1.id < n2.id')
+            ->andWhere("n1.$left = n2.$left OR n1.$right = n2.$right OR n1.$left = n2.$right OR n1.$right = n2.$left");
+
+        $result = $queryBuilder->getQuery()->execute();
+        return (int) $result[0]['count'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getWrongParentCount() :int
+    {
+        $left = $this->leftParameterName();
+        $right = $this->rightParameterName();
+        $parentId = $this->parentIdParameterName();
+
+        $queryBuilder = $this->createQueryBuilder();
+
+        $queryBuilder
+            ->select('count(distinct(c.id)) as count')
+            ->from($this->getEntityName(), 'c')
+            ->from($this->getEntityName(), 'p')
+            ->from($this->getEntityName(), 'i')
+            ->andWhere("c.$parentId = p.id")
+            ->andWhere('i.id <> p.id')
+            ->andWhere('i.id <> c.id')
+            ->andWhere("c.$left not between p.$left and p.$right OR 
+            (c.$left between i.$left and i.$right AND i.$left between p.$left and p.$right)");
+
+        $result = $queryBuilder->getQuery()->execute();
+        return (int) $result[0]['count'];
+    }
+
+    /**
+     * @return $this
+     */
+    protected function getMissingParentCount(): int
+    {
+        $parameterName = $this->parentIdParameterName();
+
+        $sub = $this->createQueryBuilder();
+        $sub->select("t.id");
+        $sub->from($this->getEntityName(),"t");
+
+        $queryBuilder = $this->createQueryBuilder();
+        $queryBuilder
+            ->select('count(1) as count')
+            ->from($this->getEntityName(), 'node')
+            ->andWhere("node.$parameterName IS NOT NULL")
+            ->andWhere($queryBuilder->expr()->not($queryBuilder->expr()->in('node.' . $parameterName, $sub->getDql())));
+
+        $result = $queryBuilder->getQuery()->execute();
+        return (int) $result[0]['count'];
+    }
+
+    public function healthStatus(): array
+    {
+        $result = [];
+        $result['oddness'] = $this->getOddnessCount();
+        $result['duplicates'] = $this->getDuplicateCount();
+        $result['wrongParent'] = $this->getWrongParentCount();
+        $result['missingParent'] = $this->getMissingParentCount();
+
+        // var_dump($result);
+
+        return $result;
+    }
+
+    public function validateTree(): bool
+    {
+        return array_sum($this->healthStatus()) == 0;
     }
 }
